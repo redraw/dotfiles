b71eff230df04ff5bd28e4bcfb13eac8c2770a92
redraw <agustinbv@gmail.com>
2020-01-21 11:32:54 -0300

    conditional install

--- .bin/setup.sh
+++ .bin/setup.sh
@@ -11,18 +11,30 @@ sudo apt-get update -qq && sudo apt-get install -qq \
 
 # oh-my-zsh
 info "Installing oh-my-zsh..."
-curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh >/dev/null
-mv $HOME/.zshrc{.pre-oh-my-zsh,}
+if [ ! -d $HOME/.oh-my-zsh ]; then
+    curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh >/dev/null
+    mv $HOME/.zshrc{.pre-oh-my-zsh,}
+else
+    echo "skipped"
+fi
 
 # spaceship-theme
 info "Installing spaceship-theme..."
 ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" >/dev/null
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+if [ ! -d $ZSH_CUSTOM/themes/spaceship-prompt ]; then
+    git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" >/dev/null
+    ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+else
+    echo "skipped"
+fi
 
 # pyenv
 info "Installing pyenv"
-curl -fsSL https://pyenv.run | bash >/dev/null
+if [ ! -d $HOME/.pyenv ]; then
+    curl -fsSL https://pyenv.run | bash >/dev/null
+else
+    echo "skipped"
+fi
 
 # install pyenv / python build deps
 info "Installing Python build dependencies..."
@@ -30,17 +42,29 @@ sudo apt-get -qq install --no-install-recommends make build-essential libssl-dev
 
 # nvm
 info "Installing nvm..."
-curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash >/dev/null
+if [ ! -d $HOME/.nvm ]; then
+    curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash >/dev/null
+else
+    echo "skipped"
+fi
 
 # docker
 info "Installing docker..."
-curl -fsSL https://get.docker.com | sh >/dev/null
-sudo usermod -aG docker $(whoami)
+if [ ! -x $(command -v docker) ]; then
+    curl -fsSL https://get.docker.com | sh >/dev/null
+    sudo usermod -aG docker $(whoami)
+else
+    echo "skipped"
+fi
 
 # docker-compose
 info "Installing docker-compose..."
-sudo curl -fsSL "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose >/dev/null
-sudo chmod +x /usr/local/bin/docker-compose
+if [ ! -x $(command -v docker-compose) ]; then
+    sudo curl -fsSL "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose >/dev/null
+    sudo chmod +x /usr/local/bin/docker-compose
+else
+    echo "skipped"
+fi
 
 # change shell to zsh
 info "Changing shell to zsh (it will ask for your user password)"
