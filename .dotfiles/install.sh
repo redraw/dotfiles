47762ca853924526bd2067a08a5f3d718bc89008
redraw <agustinbv@gmail.com>
2022-02-19 11:41:45 -0300

    font fix

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 GIT_DIR="$HOME/.dotfiles.git"
 
--- .dotfiles/recipes/fonts.sh
+++ .dotfiles/recipes/fonts.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-sudo apt install -qq fonts-powerline
+FONT=RobotoMono
+
+(
+    cd $HOME/.local/share/fonts
+    curl -L https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/$FONT.zip -o $FONT.zip
+    unzip $FONT.zip && rm $FONT.zip
+)
